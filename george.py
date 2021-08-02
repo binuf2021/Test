8f4eb5fe9a9fed279fc3b8ff02784e08d2ebc56c
binuf2021 <binuf.varghese07@gmail.com>
2021-08-02 17:25:35 +0530

    Connected ec2 instance with gitbash and git bash with github

--- george.py
+++ george.py
@@ -1,3 +1,6 @@
+Now i am using git bash as CLI and i have clonned github on gitbash, Using AWS instance.
+
+
 The quick black fox jumps over the lazy brown dog.
 The quick black fox jumps over the lazy brown dog.
 
