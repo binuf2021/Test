815f6479b82f51965c4c7a58a6819ee2d74300df
binuf2021 <binuf.varghese07@gmail.com>
2021-08-02 14:25:08 +0530

    Added new lines

---
+++ george.py
@@ -0,0 +1,10 @@
+The quick black fox jumps over the lazy brown dog.
+The quick black fox jumps over the lazy brown dog.
+
+
+
+The quick brown fox jumps over the lazy black dog.
+The quick brown fox jumps over the lazy black dog.
+The quick brown fox jumps over the lazy black dog.
+The quick brown fox jumps over the lazy black dog.
+The quick brown fox jumps over the lazy black dog.
